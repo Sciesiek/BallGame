835d6886e786d9efc37b63041e25c2916fa4552c
Sciechu <26565@pwsz-kalisz.pl>
2020-05-18 18:38:16 +0200

    Bouncing ball initial physics

---
+++ Assets/Script/BallBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float minX;
+    public float minY;
+    public float maxX;
+    public float maxY;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.right * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    Vector2 GetRandomDirection()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        return new Vector2(randomX, randomY);
+    }
+}
---
+++ Assets/Script/PlatformsControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlatformsControl : MonoBehaviour {
+    
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+}
